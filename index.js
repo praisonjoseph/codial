f93fba41ca9c4fe3e72b68f9c780f293a7580f1a
praisonjoseph <praison.joseph@gmail.com>
2023-03-28 17:01:42 +0530

    setup of view engine

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 8000;
+const path = require('path')
 
 // app routes
 app.use('/', require('./routes/index'))
 
+// setup the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 app.listen(port, function(err){
     if (err) {
